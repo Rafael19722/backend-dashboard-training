88b213f97c380be5b842c81fe750de1ffa68faff
Rafael Rangel <rafinhad3las@gmail.com>
2026-02-21 17:55:16 -0300

    feat(docs): setup swagger OpenAPI generation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,17 +1,34 @@
 import { IsString, IsNumber, IsUrl, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProductDto {
+    @ApiProperty({
+        description: 'The name of the product',
+        example: 'Teclado Mecânico',
+    })
     @IsString()
     @IsNotEmpty()
     title: string;
 
+    @ApiProperty({
+        description: 'The price in USD/BRL',
+        example: 350.50,
+    })
     @IsNumber()
     price: number;
 
+    @ApiProperty({
+        description: 'Product category',
+        example: 'Periféricos',
+    })
     @IsString()
     @IsNotEmpty()
     category: string;
 
+    @ApiProperty({
+        description: 'URL of the product image',
+        example: 'https://minhaloja.com/imagens/teclado.jpg',
+    })
     @IsUrl()
     @IsNotEmpty()
     imageUrl: string;
