6526d2175542491b83c6cd105188d0b67193fdec
Rafael Rangel <rafinhad3las@gmail.com>
2026-02-21 16:50:32 -0300

    feat(database): implement global Prisma module and service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, OnModuleInit, onModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    //start
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    //shutdown
+    async onModuleDestroy() {
+        await this.$disconnect();
+    }
+}
